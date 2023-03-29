48280c0835cea975740d6d7f0135972262ca4807
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2023-03-29 15:54:15 +0530

    implemented Concern to manage image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,13 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :subtitle, :body
+
+	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+		self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
+	end
 end
