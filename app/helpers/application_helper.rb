9ab61c5aaae15a3adce421927f18546987264164
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2023-04-05 12:42:59 +0530

    Implemented helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def sample_helper
+		"<p> My sample helper </p>".html_safe
+	end
+
+	def login_helper
+		if current_user.is_a?(User)
+      		link_to 'Logout', destroy_user_session_path, method: :delete, data: {confirm: 'Do you wanna logout'}
+    	else
+	      	(link_to 'Login', new_user_session_path) + "<br>".html_safe  + (link_to 'Signup', new_user_registration_path)
+    	end
+	end
 end
