ecace8d8ef24d62475226bb7b7bdd9a15fa92b20
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2023-04-12 15:05:25 +0530

    Authorization commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
 	end
 
 	def login_helper
-		if current_user.is_a?(User)
-      		link_to 'Logout', destroy_user_session_path, method: :delete, data: {confirm: 'Do you wanna logout'}
+		if current_user.is_a?(GuestUser)
+      		(link_to 'Login', new_user_session_path) + "<br>".html_safe  + (link_to 'Signup', new_user_registration_path)
     	else
-	      	(link_to 'Login', new_user_session_path) + "<br>".html_safe  + (link_to 'Signup', new_user_registration_path)
+	      	link_to 'Logout', destroy_user_session_path, method: :delete, data: {confirm: 'Do you wanna logout'}
     	end
 	end
 
